69e596fe62cafd351c872f797010a5284685df0d
martingom04 <martingomez9636@gmail.com>
2025-01-24 16:23:52 -0500

    Descarga de migraciones de flask

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,19 +2,18 @@
 from flask import Flask
 from app.db import db_instance, db
 from config import Config  # Importamos la configuración
-#TODO instalar flask-migrate
 from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
 
     # Cargar configuración desde config.py
-    app.config.from_object(Config) # cargamos la configuracion desde el archivo config.py que acabamos de crear 
+    app.config.from_object(Config) # cargamos la configuracion desde el archivo config.py que acabamos de crear
 
     # Inicializar la base de datos
     db_instance.init_app(app)
 
     # Configurar Flask-Migrate para manejar migraciones
-    Migrate(app, db)
+    Migrate(app, db) #hacemos la migracion de la base de datos 
 
     return app  # Retornar la aplicación sin crear tablas manualmente
