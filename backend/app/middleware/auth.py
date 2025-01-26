08c7bef375d681a7adb60001d6e1a3e4d6dabd69
martingom04 <martingomez9636@gmail.com>
2025-01-25 21:05:44 -0500

    Comentarios en el __init__

--- backend/app/middleware/auth.py
+++ backend/app/middleware/auth.py
@@ -1,4 +1,5 @@
 # lo que tengo que investigar es como se usa esto y como funciona esto para poder tener seguridad
+#documentacion para la mejora de este codigo 
 from repositories import UserRepository
 
 
